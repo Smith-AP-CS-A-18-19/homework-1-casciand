c8231528293f63f75f1a1ee1b82732eea2f40209
casciand <casciand@berkeleyprep.org>
2018-09-14 14:32:02 -0400

    COmplete

--- Homework1.java
+++ Homework1.java
@@ -11,34 +11,38 @@ public class Homework1 {
 	public static int problem1() {
 		int mystery = 1;
 		mystery = mystery + 1;
-		int mystery = 1 - 2 * mystery;
-		return mystery;
-		
+		int mysteryTwo = 1 - 2 * mystery;
+		return mysteryTwo;
 	}
 
 	/* Construct and return a Rectangle that has a width of w and a
 	 * height of h
 	 */
 	public static Rectangle problem2(int w, int h) {
-
+		Rectangle rect = new Rectangle(w, h);
+		return rect;
 	}
 
 	/* Declare an integer, assign it a value of 5, then return it
 	 */
 	public static int problem3() {
-
+		int five = 5;
+		return five;
 	}
 
 	/* Declare and return a String containing the phrase "Computer Science"
 	 */
 	public static String problem4() {
-
+		String compsci = "Computer Science";
+		return compsci;
 	}
 
 	/* Add 5 to num and return the new value
 	 */
 	public static int problem5(int num) {
-
+		int numb = 7;
+		numb += 5;
+		return numb;
 	}
 
 }
